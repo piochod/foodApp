0c413763b1cc0f2b0db1968b33ac1f420512583b
Jakub Dolecki <jakub.dolecki2000@gmail.com>
2024-06-22 17:35:08 +0200

    t

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import ChooseDiet from './components/ChooseDiet';
+import Login from './components/Login';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Cookies from 'js-cookie';
 import { useState } from 'react';
 
 function App() {
 
   const [menuOpen,setMenuOpen] = useState(false);
+  const accessToken = Cookies.get('accessToken');
 
   return (
     <div className="App">
       <NavBar menuOpen={menuOpen} setMenuOpen={() => setMenuOpen(!menuOpen)}/>
-      <ChooseDiet />
+      <Login />
       <Footer />
     </div>
   );
